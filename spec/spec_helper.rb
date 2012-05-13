3331a1d65fd2120543c195445482d94a02e77a5e
Junya Ogura <junyaogura@gmail.com>
2012-05-13 20:46:19 +0900

    add configuration for coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,12 @@
-# -*- coding: utf-8 -*-
+require 'simplecov'
+SimpleCov.start do
+  add_filter '.bundle/'
+  add_filter 'spec/'
+end
 
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$:.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+
+require 'rspec'
 
 require 'alloy'
 
