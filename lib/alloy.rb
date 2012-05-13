61cfeab411d91b9fe54cd7a3e1eaa890ae3d7904
Junya Ogura <junyaogura@gmail.com>
2012-05-13 20:56:04 +0900

    change require to autoload

--- lib/alloy.rb
+++ lib/alloy.rb
@@ -3,13 +3,14 @@
 $LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__))
 
 module Alloy
+  autoload :Config, 'alloy/config'
+  autoload :Task, 'alloy/task'
+
+  module Command
+    autoload :Build, 'alloy/command/build'
+    autoload :Clean, 'alloy/command/clean'
+    autoload :CoffeeScript, 'alloy/command/coffee_script'
+  end
 
   PERL_COLOR_FILTER = %Q{| perl -pe 's/^\\[DEBUG\\].*$/\\e[35m$&\\e[0m/g;s/^\\[INFO\\].*$/\\e[36m$&\\e[0m/g;s/^\\[WARN\\].*$/\\e[33m$&\\e[0m/g;s/^\\[ERROR\\].*$/\\e[31m$&\\e[0m/g;'}
-
 end
-
-require "alloy/command/build"
-require "alloy/command/clean"
-require "alloy/command/coffee_script"
-require "alloy/config"
-require "alloy/task"
